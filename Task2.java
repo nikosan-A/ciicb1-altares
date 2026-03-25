79209d2d8aeeaee4cafd40a032172aa6cac1c3a0
nikosan-A <francis.nikko.28@gmail.com>
2026-03-25 12:04:50 +0800

    Update Task2.java

--- Task2.java
+++ Task2.java
@@ -1,1 +1,18 @@
+public class Task2 {
+    public static void main(String[] args) {
+        // primitives
+        char H = 'H';
+        byte three = 3;
+        int one = 1;
+        short zero = 0;
+        float twoPointZero = 2.0f;
+        boolean truth = true;
 
+        // build the output string
+        String output = "" + H + three + one + one + zero + " w" + zero + "r" + one + "d " 
+                        + twoPointZero + " " + truth;
+
+        // print result
+        System.out.println(output);
+    }
+}
