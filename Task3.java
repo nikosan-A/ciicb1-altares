a53bae312e40d3ae7e23c1a7f1f5d966fb1935fd
nikosan-A <francis.nikko.28@gmail.com>
2026-03-25 12:12:49 +0800

    Fix output string format in Task2.java
    Updated output string to reflect the correct format.

---
+++ Task3.java
@@ -0,0 +1,17 @@
+public class Task3 {
+    public static void main(String[] args) {
+        
+String a = "Wow";          // use the same literal
+String b = "Wow";          // same reference from string pool
+String c = "Boo";    // must differ from a
+String d = "Wow!";         // matches b + "!"
+
+boolean b1 = a == b;                  // true (same reference in pool)
+boolean b2 = d.equals(b + "!");       // true ("Wow!" == "Wow" + "!")
+boolean b3 = !c.equals(a);            // true ("Boo" != "Wow")
+
+if (b1 && b2 && b3) {
+    System.out.println("Success!");
+}
+    }
+}
