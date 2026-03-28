14278399ffdccb297996bd497760aeaa2e43ed87
nikosan-A <francis.nikko.28@gmail.com>
2026-03-28 00:45:53 +0000

    update

--- Task1.java
+++ Task1.java
@@ -2,8 +2,8 @@
  * The HelloWorldApp class implements an application that
  * simply prints "Hello World!" to standard output.
  */
-clas helloWorldApp {
+class helloWorldApp {
     public static void main(String[] args) {
-        System.out.rintln("Kamusta Mundo!); // Display the string.
+        System.out.println("Kamusta Mundo!"); // Display the string.
     }
 }
