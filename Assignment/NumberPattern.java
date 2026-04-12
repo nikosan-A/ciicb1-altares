adc06770014b638c96afc011c06e0745c7b7ecd1
nikosan-A <francis.nikko.28@gmail.com>
2026-04-12 17:54:55 +0000

    99

---
+++ Assignment/NumberPattern.java
@@ -0,0 +1,22 @@
+package Assignment;
+
+import java.util.Scanner;
+
+public class NumberPattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter an integer: ");
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+              sc.close();
+
+            
+        }
+    }
+}
