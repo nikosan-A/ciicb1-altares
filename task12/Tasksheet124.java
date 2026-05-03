e3763067bc420490b00ac9c3c1a5df3762385ff8
nikosan-A <francis.nikko.28@gmail.com>
2026-05-03 05:29:40 +0000

    task12

---
+++ task12/Tasksheet124.java
@@ -0,0 +1,32 @@
+package task12;
+
+class Point {
+    private double x;
+    private double y;
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void print() {
+        System.out.println("(" + x + ", " + y + ")");
+    }
+
+    // Your code goes here
+    public void scale() {
+        // Divide both coordinates by 2 to move halfway toward (0,0)
+        x = x / 2;
+        y = y / 2;
+    }
+}
+
+public class Tasksheet124 {
+    public static void main(String[] args) {
+        Point p = new Point(32, 32);
+        for (int i = 0; i < 5; i++) {
+            p.scale();
+            p.print();
+        }
+    }
+}
